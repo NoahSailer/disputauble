69d9994564d14725df1b3730bb1e38aa62a55ac2
NoahSailer <nsailer9@gmail.com>
2025-04-08 22:54:46 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# disputable
+
+Clone this repo and run `create_cobaya_env.sh`. Here's an example for submitting a bunch of minimizing jobs
+
+```
+sbatch minimize.sh pact_tau\=0.06
+sbatch minimize.sh pact_tau\=0.09
+sbatch minimize.sh pact_tau\=0.06_w0wa
+sbatch minimize.sh pact_tau\=0.09_w0wa
+```
---
+++ minimize.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -l
+#SBATCH -J minimize
+#SBATCH -t 4:00:00
+#SBATCH -N 1
+#SBATCH -o log/minimize.out
+#SBATCH -e log/minimize.err
+#SBATCH -q regular
+#SBATCH -C cpu
+#SBATCH -A desi
+
+name=$1
+conda activate cobaya_up2d8
+export COBAYA_USE_FILE_LOCKING=False
+export OMP_NUM_THREADS=4
+srun -N 1 -n 32 -c 4 cobaya-run --minimize --force ${name}.yaml
